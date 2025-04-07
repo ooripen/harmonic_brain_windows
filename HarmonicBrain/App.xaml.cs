8addaba29562b6239c6e56ef109ec26e56eecf69
Oori Pen <160356646+ooripen@users.noreply.github.com>
2025-04-07 09:30:21 +0300

    Delete HarmonicBrain directory

--- HarmonicBrain/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-using System.Windows;
-
-namespace HarmonicBrain
-{
-    public partial class App : Application
-    {
-        // You can add startup initialization here (e.g., load saved settings).
-    }
-}
